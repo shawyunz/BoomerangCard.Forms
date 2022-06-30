36f6305d893626c0d1c3a89ea99a13c0a6bd7629
shawyunz <shawyunz@gmail.com>
2022-07-01 10:50:10 +1200

    Add CardView, ready for more cards and shorten the code.

---
+++ BoomerangCard.Forms/BoomerangCard.Forms/CardView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BoomerangCard.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardView : ContentView
+    {
+        public static readonly BindableProperty CardColorProperty =
+            BindableProperty.Create(nameof(CardColor), typeof(Color), typeof(CardView), Color.White, BindingMode.TwoWay);
+
+        public CardView()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public Color CardColor
+        {
+            get => (Color)GetValue(CardColorProperty);
+            set => SetValue(CardColorProperty, value);
+        }
+    }
+}
