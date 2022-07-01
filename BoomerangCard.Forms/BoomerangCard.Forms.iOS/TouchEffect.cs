ff99287fb7cb5cfa79b5256b61aaa8b43da5dcbb
shawyunz <shawyunz@gmail.com>
2022-07-01 17:51:31 +1200

    v6: Fix glitches on ios.

---
+++ BoomerangCard.Forms/BoomerangCard.Forms.iOS/TouchEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("XamarinDocs")]
+[assembly: ExportEffect(typeof(BoomerangCard.Forms.iOS.TouchEffect), "TouchEffect")]
+
+namespace BoomerangCard.Forms.iOS
+{
+    public class TouchEffect : PlatformEffect
+    {
+        private TouchRecognizer touchRecognizer;
+        private UIView view;
+
+        protected override void OnAttached()
+        {
+            // Get the iOS UIView corresponding to the Element that the effect is attached to
+            view = Control == null ? Container : Control;
+
+            // Uncomment this line if the UIView does not have touch enabled by default
+            //view.UserInteractionEnabled = true;
+
+            // Get access to the TouchEffect class in the .NET Standard library
+            Effect.TouchEffect effect = (Effect.TouchEffect)Element.Effects.FirstOrDefault(e => e is Effect.TouchEffect);
+
+            if (effect != null && view != null)
+            {
+                // Create a TouchRecognizer for this UIView
+                touchRecognizer = new TouchRecognizer(Element, view, effect);
+                view.AddGestureRecognizer(touchRecognizer);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            if (touchRecognizer != null)
+            {
+                // Clean up the TouchRecognizer object
+                touchRecognizer.Detach();
+
+                // Remove the TouchRecognizer from the UIView
+                view.RemoveGestureRecognizer(touchRecognizer);
+            }
+        }
+    }
+}
